3b048464e26558f3838b453c180cd6a976e400ac
2231482 <142741738+2231482@users.noreply.github.com>
2024-08-31 10:33:19 +0000

    data

---
+++ 01_basic/test.js
@@ -0,0 +1,11 @@
+const accountid=123112;
+let accountemail = "abhinawkumar@gmail.com";
+var accountpassword ="121212";
+accountpassword=23;
+// let accountid=23; constant values are can't change
+console.log(accountid);
+console.log(accountid.length);
+console.table([accountemail,accountid,accountpassword]);
+
+/*prefer not to use var beacuse of issue in block scope and functional scope*/
+
