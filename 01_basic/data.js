7572cebccb4eb4ad892676230e9c6481f651150f
2231482 <142741738+2231482@users.noreply.github.com>
2024-08-31 19:28:52 +0000

    datatypes

---
+++ 01_basic/data.js
@@ -0,0 +1,11 @@
+//data type 2 to power 53
+//big int
+//String =""
+//boolean = true /falses
+//null= there is not a value
+//undefined -> value not defined yet
+//symbol => unique
+console.log(typeof "data"); //string //to see which type of data 
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
+console.log(typeof 23);//number
