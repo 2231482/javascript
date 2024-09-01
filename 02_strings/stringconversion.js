c7716eb697f5f066f5a2528d06255095da723214
2231482 <142741738+2231482@users.noreply.github.com>
2024-09-01 06:25:22 +0000

    strings conversion

---
+++ 02_strings/stringconversion.js
@@ -0,0 +1,16 @@
+// console.log("1"+2);//12
+// console.log(1 +"2");//12
+// console.log("1"+2+ 1);//121
+// console.log(2+1+"2");//32
+// let x= null;
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);//null is greater than 0 and equal to zero
+
+// console.log(undefined>0);
+// console.log(undefined==0);
+// console.log(undefined>=0);//undefined has not defined has noa ny value.
+
+// === sign
+console.log("2"===2); //false beacause it checks the type also 
+
