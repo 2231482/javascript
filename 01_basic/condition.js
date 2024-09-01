0376394e3622929d219940b31b30f170b30c27e2
2231482 <142741738+2231482@users.noreply.github.com>
2024-09-01 05:42:29 +0000

    condition statesments

---
+++ 01_basic/condition.js
@@ -0,0 +1,47 @@
+//condition statesments in javascript.
+
+//if -else statement
+
+// let age=90
+// if (age>18) {
+//     console.log("you are adult");
+//     }
+// else{
+//     console.log("you are not adult")
+// }
+
+//nested if statement
+
+// let x=12;
+// if(x>12){
+//     console.log("big");
+// }
+// else if(x<12){
+//     console.log("smaller");
+// }
+// else{
+//     console.log("equal");
+// }
+
+//ternary operator
+
+// let x=12;
+// let otpt=(x>12)?("x is bigger than 12"):("x is smaller than 12");
+// console.log(otpt);
+
+// switch statement
+
+// let day =1;
+// switch (day) {
+//     case 1:
+//         console.log("monday");
+        
+        
+//         break;
+
+//     default:
+//         console.log("sunday");
+        
+//         break;
+// }
+
