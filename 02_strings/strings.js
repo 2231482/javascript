af785524c34989b02842b3e611499e51f00c4c59
2231482 <142741738+2231482@users.noreply.github.com>
2024-09-03 17:10:58 +0000

    strings

---
+++ 02_strings/strings.js
@@ -0,0 +1,28 @@
+// let str ="abhinaw";
+// let str1="raju";
+// console.log(str.length);
+// console.log(str[2+1]);
+// console.log(str,str2);
+
+//uppercase //js is a case sensitive language.
+// let str2="lalu PraShAd";
+// let abc=str2.toUpperCase();
+
+//lower case 
+// let acb=str2.toLowerCase();
+// console.log(acb);
+
+//string.trim() to avoid unused space.
+// let str4="         hari          ";
+// console.log(str4);
+// console.log(str4.trim());
+
+//slicefunction str.slice(3,5); to get required strings from a strings.
+// let st="abhinaw singh";
+// console.log(st.slice(3,6));
+
+
+
+
+
+
